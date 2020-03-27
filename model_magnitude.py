5e4a8ff171a2ef0d4d5ec119086bbbcedb99a0e9
Joseph Rose <joeyrose1999@gmail.com>
2020-03-27 16:26:15 -0700

    got results of 10 queries, performance improvements
    removed stopwords from query and punctuation from documents

--- model_magnitude.py
+++ model_magnitude.py
@@ -3,26 +3,34 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import math
 from scipy import spatial
 import numpy as np
+from nltk.corpus import stopwords
+import string
 
 def termFrequency(term, doc):       
     # Splitting the document into individual terms 
-    normalizeTermFreq = doc.lower().split()  
+    translator = str.maketrans('', '', string.punctuation)
+    doc = doc.translate(translator)
+    normalizeTermFreq = doc.split()  
   
     # Number of times the term occurs in the document 
     term_in_document = normalizeTermFreq.count(term.lower())  
-  
+    # if term_in_document == 3:
+    #     print(document)
+
     #normalize value
     if term_in_document > 0:
-        term_in_document = 1 + math.log(term_in_document)  
+        term_in_document = 1 + math.log(term_in_document)
   
-    return term_in_document 
+    return term_in_document
 
 def inverseDocumentFrequency(term, allDocs): 
     num_docs_with_given_term = 0
 
     # Iterate through all the documents 
     for doc in allDocs: 
-        if term.lower() in doc.lower().split(): 
+        translator = str.maketrans('', '', string.punctuation)
+        doc = doc.translate(translator)
+        if term.lower() in doc.split(): 
             num_docs_with_given_term += 1
   
     if num_docs_with_given_term > 0: 
@@ -42,41 +50,43 @@ data.columns = ["transcript", "url"]
 #get a list of transcipts to calculate the idf values
 documents = data["transcript"].tolist()
 
-phrase = input("Please enter a query to search: ")
-phraseList = phrase.split()
-tmpList = [None] * len(phraseList)
+phrase = input("Please enter a query to search: ").lower()
+#create list of words in query to calculate the query's idf values, remove stopwords from it
+tmpList = phrase.split()
+stopwords = set(stopwords.words('english')) 
+phraseList = []
+for word in tmpList:
+    if word not in stopwords:
+        phraseList.append(word)
+print(phraseList)
+
+#don't need to find the tf-idf values for the query since we're only evaluating the magnitudes of the document vectors!
 phrase_idfs = [None] * len(phraseList)
 for i in range(len(phraseList)):
-    phrase_tf = termFrequency(phraseList[i], phrase)
     phrase_idf = inverseDocumentFrequency(phraseList[i], documents)
     #since the idf is how many documents this word appears in, just calculate it once and put it in a list!
     phrase_idfs[i] = phrase_idf
-    tmpList[i]  = phrase_tf * phrase_idf
-
-phrase_tfidf = tmpList
-
-data = data.values.tolist()
 
 class Document():
 	def __init__(self, link, magnitude):
 		self.link = link
 		self.magnitude = magnitude
 
+#holds objects with magnitude values and corresponding link to TED talk
 documents = []
+data = data.values.tolist()
 
 for array in data:
-    document = array[0]
+    document = array[0].lower()
     link = array[1]
-    tmpList = [None] * len(phraseList)
+    document_tfidfs = [None] * len(phraseList)
     for i in range(len(phraseList)):
         document_tf = termFrequency(phraseList[i], document)
         #print("the phrase {} has a frequency of {}".format(phraseList[i], document_tf))
         document_idf = phrase_idfs[i]
-        tmpList[i]  = document_tf * document_idf
-    document_tfidf = tmpList
-    magnitude = np.linalg.norm(document_tfidf)
+        document_tfidfs[i]  = document_tf * document_idf
+    magnitude = np.linalg.norm(document_tfidfs)
     documents.append(Document(link, magnitude))
-    
 
 #sort documents by their magnitudes
 #One line sort function method using an inline lambda function lambda x: x.date
@@ -84,7 +94,7 @@ documents.sort(key=lambda x: x.magnitude, reverse=True)
 
 #print out top 10 most relavent talks, if possible
 for i in range(10):
-    if documents[i].magnitude != 0.0:
+    if documents[i].magnitude > 0.0:
         print("The talk {} had a magnitude of {}".format(documents[i].link, documents[i].magnitude))
     else:
         break
